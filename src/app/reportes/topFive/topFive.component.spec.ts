3b01cce33e503543f11a49c0207cabe274458d01
nicovio <nicolasviotti90@gmail.com>
2019-11-06 05:28:29 -0300

    WIP: topFive component.

---
+++ src/app/reportes/topFive/topFive.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { TopFiveComponent } from './topFive.component';
+
+describe('TopFiveComponent', () => {
+  let component: TopFiveComponent;
+  let fixture: ComponentFixture<TopFiveComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TopFiveComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TopFiveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/reportes/topFive/topFive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'topFive',
+  templateUrl: './topFive.component.html',
+  styleUrls: ['../reportes.component.css']
+})
+export class TopFiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
