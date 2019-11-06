ac3cc6d8a488023d6113f3ac9e07443c1cad32db
nicovio <nicolasviotti90@gmail.com>
2019-11-06 05:34:52 -0300

    Move attributes from reportes to totalizador.

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Categoria } from '../domain/Categoria';
 
 @Component({
   selector: 'app-reportes',
@@ -8,19 +7,8 @@ import { Categoria } from '../domain/Categoria';
 })
 export class ReportesComponent implements OnInit {
 
-  listado_categorias: Categoria[]
-  fecha_desde: Date
-  fecha_hasta: Date
-  categoria: Categoria
-
   constructor() { }
 
-  ngOnInit() {
-    try {
-      this.listado_categorias = [new Categoria('Rock', 'Genero musical'), new Categoria('Accion', 'Peliculas trepidantes')]
-    } catch (error) {
-      console.log(error) // mostrar errores
-    }
-  }
+  ngOnInit() { }
 
 }
--- src/app/reportes/totalizador/totalizador.component.ts
+++ src/app/reportes/totalizador/totalizador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/domain/Categoria';
 
 @Component({
   selector: 'totalizador',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../reportes.component.css']
 })
 export class TotalizadorComponent implements OnInit {
+  fecha_desde: Date
+  fecha_hasta: Date
+  listado_categorias: Categoria[]
+  categoria: Categoria
 
   constructor() { }
 
   ngOnInit() {
+    try {
+      this.listado_categorias = [new Categoria('Rock', 'Genero musical'), new Categoria('Accion', 'Peliculas trepidantes')]
+    } catch (error) {
+      console.log(error) // mostrar errores
+    }
   }
 
 }
