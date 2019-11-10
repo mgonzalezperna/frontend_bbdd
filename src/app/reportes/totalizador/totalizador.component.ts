51adca37e8d9c4333616c8b5d8a6bff4d3ba1092
nicovio <nicolasviotti90@gmail.com>
2019-11-10 20:31:19 -0300

    Add form validations to totalizador component.

--- src/app/reportes/totalizador/totalizador.component.ts
+++ src/app/reportes/totalizador/totalizador.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Categoria } from 'src/app/domain/Categoria';
-import { CategoriasService } from 'src/app/services/categoria.service';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Categoria } from 'src/app/domain/Categoria';
+import { ReporteDescargas } from 'src/app/domain/ReporteDescargas';
+import { CategoriasService } from 'src/app/services/categoria.service';
+import { ReporteService } from 'src/app/services/reporte.service';
 
 @Component({
   selector: 'totalizador',
@@ -13,23 +16,54 @@ export class TotalizadorComponent implements OnInit {
   fecha_hasta: Date
   listado_categorias: Categoria[]
   categoria: Categoria
+  fechaDesdeValidator: FormControl = new FormControl('', [Validators.required]);
+  fechaHastaValidator: FormControl = new FormControl('', [Validators.required]);
+  categoriaValidator: FormControl = new FormControl('', [Validators.required]);
+  server_error: String = ""
+  reportes_descargas: ReporteDescargas[]
+  cargando: boolean = false
 
-  constructor(private categoriaService: CategoriasService, private router: Router) { }
+  constructor(private categoriaService: CategoriasService, private reporteService: ReporteService) { }
 
   async ngOnInit() {
     try {
       this.listado_categorias = await this.categoriaService.solicitarListadoCategorias()
-      this.listado_categorias.push(new Categoria("","Todos"))
+      this.listado_categorias.push(new Categoria("", "Todos"))
     }
-     catch (error) {
+    catch (error) {
+      this.server_error = error
       console.log(error) // mostrar errores
     }
   }
-  
+
+  get error() {
+    return this.fechaDesdeMayorAFechaHastaError ? this.fechaDesdeMayorAFechaHastaError : this.server_error
+  }
+
+  get fechaDesdeMayorAFechaHastaError() {
+    return this.fechaDesdeMayorAFechaHasta() ? 'Fecha desde debe ser anterior a fecha hasta' : ''
+  }
+
+  get errorMsgFechaDesde() {
+    return this.fechaDesdeValidator.hasError('required') ? "Debe ingresar fecha desde" : ''
+  }
+
+  get errorMsgFechaHasta() {
+    return this.fechaHastaValidator.hasError('required') ? "Debe ingresar fecha hasta" : ''
+  }
+
+  fechaDesdeMayorAFechaHasta() {
+    return this.fecha_desde > this.fecha_hasta
+  }
+
+  cantSubmit() {
+    return this.fechaDesdeMayorAFechaHasta() || this.fechaDesdeValidator.hasError('required') || this.fechaHastaValidator.hasError('required') || this.categoriaValidator.hasError('required')
+  }
+
   fechaDesdeFormateada() {
     return formatearFecha(this.fecha_desde)
   }
-  
+
   fechaHastaFormateada() {
     return formatearFecha(this.fecha_hasta)
   }
@@ -42,13 +76,13 @@ export class TotalizadorComponent implements OnInit {
 }
 
 function formatearFecha(fecha: Date) {
-    // AA-MM-DD
-    return `${fecha.getFullYear()}-${agregarCero(
-      fecha.getMonth() + 1
-    )}-${agregarCero(fecha.getDate())}`;
-  }
+  // AA-MM-DD
+  return `${fecha.getFullYear()}-${agregarCero(
+    fecha.getMonth() + 1
+  )}-${agregarCero(fecha.getDate())}`;
+}
 
 function agregarCero(dia) {
-    if (dia < 10) return `0${dia}`;
-    else return dia;
-  }
+  if (dia < 10) return `0${dia}`;
+  else return dia;
+}
