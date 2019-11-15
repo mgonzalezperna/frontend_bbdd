3dac845bedf8073e4321c0c4524ef6e1792576c1
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2019-11-15 18:37:06 -0300

    Replace old tables with new sortables ones.

--- src/app/reportes/top-five/top-five-list/top-five-list.component.ts
+++ src/app/reportes/top-five/top-five-list/top-five-list.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 import { Experiencia } from 'src/app/domain/Experiencia';
-import { ReporteService } from 'src/app/services/reporte.service';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'top-five-list',
   templateUrl: './top-five-list.component.html',
   styleUrls: ['./top-five-list.component.css']
 })
-export class TopFiveListComponent implements OnInit {
-
+export class TopFiveListComponent implements OnChanges, OnInit {
+  displayed_columns: String[] = ['titulo', 'puntaje', 'comentario']
   @Input() top_five_experiencias: Experiencia[]
+  experiencias: MatTableDataSource<Experiencia>
+  
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
-  async ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    this.experiencias = new MatTableDataSource(this.top_five_experiencias)
+    this.experiencias.sort = this.sort;
+  }
 
+  ngOnInit() {
+    
   }
 
 }
--- src/app/reportes/totalizador/totalizador-list/totalizador-list.component.ts
+++ src/app/reportes/totalizador/totalizador-list/totalizador-list.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { ReporteDescargas } from 'src/app/domain/ReporteDescargas';
 import { ReporteService } from 'src/app/services/reporte.service';
 import { Categoria } from 'src/app/domain/Categoria';
 import { ActivatedRoute } from '@angular/router';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'totalizador-list',
   templateUrl: './totalizador-list.component.html',
   styleUrls: ['./totalizador-list.component.css']
 })
-export class TotalizadorListComponent implements OnInit {
-
+export class TotalizadorListComponent implements OnChanges {
+  displayed_columns: String[] = ['archivo', 'puntaje', 'velocidad', 'total']
   @Input() reportes_descargas: ReporteDescargas[]
+  info_descargas: MatTableDataSource<ReporteDescargas>
+    
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() {
   }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    this.info_descargas = new MatTableDataSource(this.reportes_descargas)
+    this.info_descargas.sort = this.sort;
   }
+
+
 
 }
